1472280e11212a5213fcb1e21d6b93db5c6969f1
samrmdhn <thesyams.rebel8@gmail.com>
2022-06-30 22:15:39 +0700

    compressimage

--- src/components/Design.vue
+++ src/components/Design.vue
@@ -9,23 +9,23 @@
         <b-row>
             <b-col lg="4" style="padding: 0; justify-content: center; align-items: center; display: flex;" class="position-relative">
                 <div class="position-absolute" style="bottom: 35px; border: 3px solid #d3ff52; border-radius: 15px; color: #d3ff52; width: 55%; text-align: center; font-weight: bolder;">EXAMPLE T-SHIRT #1</div>
-                <img class="img-fluid" src="../assets/1.jpg" alt="">
+                <img class="img-fluid" src="../assets/1 updated.jpg" alt="">
             </b-col>
             <b-col lg="4" style="padding: 0; justify-content: center; align-items: center; display: flex;" class="position-relative">
                 <div class="position-absolute" style="bottom: 35px; border: 3px solid #d3ff52; border-radius: 15px; color: #d3ff52; width: 55%; text-align: center; font-weight: bolder;">EXAMPLE T-SHIRT #2</div>
-                <img class="img-fluid" src="../assets/5.jpg" alt="">
+                <img class="img-fluid" src="../assets/5 updated.jpg" alt="">
             </b-col>
             <b-col lg="4" style="padding: 0; justify-content: center; align-items: center; display: flex;" class="position-relative">
                 <div class="position-absolute" style="bottom: 35px; border: 3px solid #d3ff52; border-radius: 15px; color: #d3ff52; width: 55%; text-align: center; font-weight: bolder;">EXAMPLE T-SHIRT #3</div>
-                <img class="img-fluid" src="../assets/6.jpg" alt="">
+                <img class="img-fluid" src="../assets/6 updated.jpg" alt="">
             </b-col>
             <div class="w-100"></div>
             <b-col style="padding: 0; justify-content: center; align-items: center; display: flex;" class="position-relative" lg="6">
             <div class="position-absolute" style="bottom: 35px; border: 3px solid #d3ff52; border-radius: 15px; color: #d3ff52; width: 55%; text-align: center; font-weight: bolder;">EXAMPLE POSTER #1</div>
-            <img class="img-fluid" src="../assets/11.jpg" alt=""></b-col>
+            <img class="img-fluid" src="../assets/11 updated.jpg" alt=""></b-col>
             <b-col style="padding: 0; justify-content: center; align-items: center; display: flex;" class="position-relative" lg="6">
             <div class="position-absolute" style="bottom: 35px; border: 3px solid #d3ff52; border-radius: 15px; color: #d3ff52; width: 55%; text-align: center; font-weight: bolder;">EXAMPLE POSTER #2</div>
-            <img class="img-fluid" src="../assets/12.jpg" alt=""></b-col>
+            <img class="img-fluid" src="../assets/12 updated.jpg" alt=""></b-col>
         </b-row>
     </b-container>
 
